44151794190beda3b8116f47299d2f79271e2413
suleman-suraj <sulemansuraj1233@gmail.com>
2021-05-10 09:19:03 +0000

    added some changes

--- routes/expensesRoute.js
+++ routes/expensesRoute.js
@@ -4,7 +4,7 @@ const protect = require("../middlewares/authMiddleware")
 const router = express.Router()
 
 router.route("/").post(createExpense).get(getAllExpenses)
-// router.route("/:_id").get(protect,getSingleExpense).put(protect,updateExpense).delete(protect,deleteExpense)
-router.route("/:_id").get(getSingleExpense).put(updateExpense).delete(deleteExpense)
+router.route("/:_id").get(protect,getSingleExpense).put(protect,updateExpense).delete(protect,deleteExpense)
+// router.route("/:_id").get(getSingleExpense).put(updateExpense).delete(deleteExpense)
 
 module.exports = router;
