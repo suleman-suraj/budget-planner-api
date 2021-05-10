dc67fc39e442b18cd4803644ce652b17f6d4dfe1
suleman-suraj <sulemansuraj1233@gmail.com>
2021-05-10 10:31:01 +0000

    added some changes

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -2,7 +2,10 @@ const Joi = require("joi")
 
 //register user validation
 const validateAddUser = new Joi.object({
-    name: Joi.string().min(3).required().max(150),
+    firstname: Joi.string().min(3).max(50).required(),
+    lastname: Joi.string().min(3).max(50).required(),
+    birthdate: Joi.string().min(3).max(50).required(),
+    phone: Joi.string().min(3).max(150).required(),
     email: Joi.string().min(10).max(200).email().required(),
     password: Joi.string().min(8).max(50).required()
 })
